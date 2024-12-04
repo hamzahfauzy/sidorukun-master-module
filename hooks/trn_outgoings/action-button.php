70eab226d130bb0083c567c6fc48d826df9c7867
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-12-04 14:15:26 +0700

    add allowed route checker

--- hooks/trn_outgoings/action-button.php
+++ hooks/trn_outgoings/action-button.php
@@ -4,12 +4,16 @@
     <i class="fa-solid fa-eye"></i> Detail
 </a>
 <?php if($data->status == 'NEW'): ?>
+<?php if(is_allowed('master/transactions/outgoings/approve', auth()->id)): ?>
 <a href="<?= routeTo('master/transactions/outgoings/approve', ['id' => $data->id]) ?>" class="btn btn-primary btn-sm" onclick="if(confirm('Apakah anda yakin approve data ini ?')){return true}else{return false}">
     <i class="fa-solid fa-check"></i> Approve
 </a>
+<?php endif ?>
+<?php if(is_allowed('master/transactions/outgoings/cancel', auth()->id)): ?>
 <a href="<?= routeTo('master/transactions/outgoings/cancel', ['id' => $data->id]) ?>" class="btn btn-danger btn-sm" onclick="if(confirm('Apakah anda yakin cancel data ini ?')){return true}else{return false}">
     <i class="fa-solid fa-time"></i> Cancel
 </a>
+<?php endif ?>
 <?php endif ?>
 <?php
 
--- hooks/trn_receives/action-button.php
+++ hooks/trn_receives/action-button.php
@@ -5,12 +5,16 @@
 </a>
 
 <?php if($data->status == 'NEW'): ?>
+    <?php if(is_allowed('master/transactions/receives/approve', auth()->id)): ?>
 <a href="<?= routeTo('master/transactions/receives/approve', ['id' => $data->id]) ?>" class="btn btn-primary btn-sm" onclick="if(confirm('Apakah anda yakin approve data ini ?')){return true}else{return false}">
     <i class="fa-solid fa-check"></i> Approve
 </a>
+<?php endif ?>
+<?php if(is_allowed('master/transactions/receives/cancel', auth()->id)): ?>
 <a href="<?= routeTo('master/transactions/receives/cancel', ['id' => $data->id]) ?>" class="btn btn-danger btn-sm" onclick="if(confirm('Apakah anda yakin cancel data ini ?')){return true}else{return false}">
     <i class="fa-solid fa-time"></i> Cancel
 </a>
+<?php endif ?>
 <?php endif ?>
 <?php
 
